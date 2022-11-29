06b8c1f99a3f439148cff3af43da28dac9457368
Sonal Varshney <sonalvrshny@gmail.com>
2022-11-28 20:36:52 -0500

    updated schema

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -2,8 +2,13 @@ import mongoose from 'mongoose';
 
 const schema = mongoose.Schema({
     tuit: String,
+    avatarIcon: {type: String, default: '../../images/zuko-profile.jpeg'},
+    username: {type: String, default: 'Prince Zuko'},
+    handle: {type: String, default: 'zukoshonor'},
     likes: {type: Number, default: 0},
     liked: {type: Boolean, default: false},
+    unlikes: {type: Number, default: 0},
+    unliked: {type: Boolean, default: false},
 }, {collection: 'tuits'});
 
 export default schema;
